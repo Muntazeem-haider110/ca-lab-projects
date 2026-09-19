9d66f1fd517d8d00602cc33981c212ea1b68b15d
Syed Muntazeem Haider Rizvi <sr12109@st.habib.edu.pk>
2026-09-19 22:12:07 +0500

    Refactor assembly code for conditional operations

--- Lab02/Risclab2(2).s
+++ Lab02/Risclab2(2).s
@@ -1,42 +1,43 @@
 .text
 .globl main
 main:
-    li x1, 1           # case1 selector
-    li x2, 2           # case2 selector
-    li x3, 3           # case3 selector
-    li x4, 4           # case4 selector
+    li x1, 0x01        # base address of save array
+    li x2, 0x02
+    li x3, 0x03
+    li x4, 0x04        # i = 2
+    li x20, 0x05       # k = 5
 
-    li x22, 2          # x (switch variable)
-    li x23, 5          # b = 5
-    li x24, 3          # c = 3
-    li x25, 0          # a = 0
+    li x22, 0x12       # b
+    li x23, 0x13       # c
 
-    beq x22, x1, case1
-    beq x22, x2, case2
+    bne x20, x4, else2 # if k != i → else2
+    add x21, x22, x23  # a = b + c
+    sw x21, 0(x1)      # store a
+    beq x0, x0, Exit   # unconditional jump
 
-    beq x22, x3, case3
-    beq x22, x4, case4
-    j default
+else2:
+    bne x20, x3, else3 # if k != l → else3
+    sub x21, x22, x23  # a = b - c
+    sw x21, 0(x1)
+    beq x0, x0, Exit
 
-case1:
-    add x25, x23, x24  # a = b + c
-    j end
+else3:
+    bne x20, x3, else4 # if k != l → else4
+    srai x21, x22, 1   # a = b / 2
+    sw x21, 0(x1)
+    beq x0, x0, Exit
 
-case2:
-    sub x25, x23, x24  # a = b - c
-    j end
-
-case3:
-    mul x25, x23, x24  # a = b * c
-    j end
-
-case4:
-    div x25, x23, x24  # a = b / c
-    j end
+else4:
+    bne x20, x4, Exit  # if k != m → Exit
+    slli x21, x22, 1   # a = b * 2
+    sw x21, 0(x1)
+    beq x0, x0, Exit
 
 default:
-    li x25, 0          # a = 0
-    j end
+    li x21, 0x00       # a = 0
+    sw x21, 0(x1)
+    beq x0, x0, Exit
 
-end:
-    j end              # halt
+Exit:
+    j Exit             # infinite loop / halt
+
